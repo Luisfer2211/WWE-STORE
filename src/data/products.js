4f251b40c03653c1afd193fd91312b48285bb541
Luisfer2211 <luisferpalacioslopez@gmail.com>
2025-06-03 13:36:44 -0600

    nuevos productos

--- src/data/products.js
+++ src/data/products.js
@@ -2,83 +2,162 @@
 export const products = [
   {
     id: 1,
-    name: "John Cena 'We Never Give Up' T-Shirt",
+    name: "John Cena Farewell Tour 2025 T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/c6caecfb-a19f-4abd-81db-03a23e3eb931.png",
-    category: ["men"],
+    image: "https://images.footballfanatics.com/john-cena/mens-green-john-cena-farewell-tour-2025-dallas-t-shirt_ss5_p-203010546+u-a3nzfgulytsa6ftinn4x+v-ram3bhvhjypnnzsj7nec.jpg?_hv=2",
+    category: ["men", "women", "kids"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
   {
     id: 2,
-    name: "Randy Orton 'Legend Killer' T-Shirt",
+    name: "Roman Reigns OTC T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/b4e36993-3a5b-4586-aa5f-6b282ee90203.png",
-    category: ["men"],
+    image: "https://images.footballfanatics.com/roman-reigns/mens-black-roman-reigns-otc-t-shirt_ss5_p-202104134+u-hiop4wolo8ichfotndjw+v-prkbsxq0hfwybyqxe5jc.jpg?_hv=2",
+    category: ["men", "kids"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
   {
     id: 3,
-    name: "Becky Lynch 'The Man' T-Shirt",
+    name: "Jey Uso Yeet T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/82490aa4-00b1-44e6-964f-87be0c3c09f2.png",
-    category: ["women"],
+    image: "https://images.footballfanatics.com/jey-uso/wwe-jey-uso-golden-yeet-t-shirt-black-mens_ss5_p-202350582+u-dy0lfwjqrbprawh0owp6+v-nkhigzylr9znes5xthve.jpg?_hv=2",
+    category: ["men", "kids"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
   {
     id: 4,
-    name: "Roman Reigns 'Head of the Table' T-Shirt",
+    name: "Cody Rhodes I Won't Give Up T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/8e3c41f1-5c74-4c49-b18d-6cd39b031b0c.png",
+    image: "https://images.footballfanatics.com/cody-rhodes/mens-black-cody-rhodes-i-wont-give-up-t-shirt_ss5_p-203039591+u-pk9elw4vw1nd3r0ncmyd+v-i0kkhiui1we6nqyrcw2b.jpg?_hv=2&w=600",
     category: ["men", "kids"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
   {
     id: 5,
-    name: "Sasha Banks 'Legit Boss' T-Shirt",
+    name: "CM Punk Best In The World T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/85437c5a-0065-4377-ae9a-d5d575535ee5.png",
-    category: ["women"],
-    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
-  },
-  {
-    id: 6,
-    name: "Edge 'Rated R Superstar' T-Shirt",
-    price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/5596e3ba-7d74-4320-8eb1-c6ef68cd648f.png",
+    image: "https://images.footballfanatics.com/cm-punk/mens-white/black-cm-punk-best-in-the-world-ringer-t-shirt_ss5_p-201184608+u-vnxayzpvzghh0hpx9txk+v-hmbzydhlk5vstiftqwqj.jpg?_hv=2",
     category: ["men"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
   {
-    id: 7,
-    name: "Edge 'Rated R Superstar' T-Shirt",
+    id: 6,
+    name: "Rhea Ripley Brutality T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/5596e3ba-7d74-4320-8eb1-c6ef68cd648f.png",
+    image: "https://images.footballfanatics.com/rhea-ripley/wwe-rhea-ripley-this-is-my-brutality-t-shirt-black-mens_ss5_p-203086577+u-cqnuqqefgnaiyr0r3f2a+v-omvgroisa726kmfvj1qx.jpg?_hv=2",
+    category: ["women"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 7,
+    name: "Karrion Kross String Them Up T-Shirt",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/karrion-kross/mens-black-karrion-kross-string-them-up-t-shirt_ss5_p-202940143+u-cjoffy2ogk0epinarcan+v-hmnkrnkuzsd078c1cnvv.jpg?_hv=2",
     category: ["men"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
   {
     id: 8,
-    name: "Edge 'Rated R Superstar' T-Shirt",
+    name: "Jacob Fatu All Gas No Brakes T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/5596e3ba-7d74-4320-8eb1-c6ef68cd648f.png",
+    image: "https://images.footballfanatics.com/jacob-fatu/wwe-jacob-fatu-all-gas-no-brakes-t-shirt-black-mens_ss5_p-202950618+u-xpydzpissapsixjkhy5h+v-fihux9gkpvwlcv2lpfjo.jpg?_hv=2",
     category: ["men"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
   {
     id: 9,
-    name: "Edge 'Rated R Superstar' T-Shirt",
+    name: "Liv Morgan Revenge Tour T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/5596e3ba-7d74-4320-8eb1-c6ef68cd648f.png",
-    category: ["men"],
+    image: "https://images.footballfanatics.com/liv-morgan/mens-black-liv-morgan-revenge-tour-t-shirt_ss5_p-201598215+u-wbypi1tf08sw6jek9p9d+v-v08vo1joswrwzc9wrn6s.jpg?_hv=2&w=600",
+    category: ["women"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
   {
     id: 10,
-    name: "Edge 'Rated R Superstar' T-Shirt",
+    name: "Stone Cold Steve Austin 3:16 T-Shirt",
     price: 24.99,
-    image: "https://cdn.usegalileo.ai/sdxl10/5596e3ba-7d74-4320-8eb1-c6ef68cd648f.png",
+    image: "https://m.media-amazon.com/images/I/41L43nBaEKL._AC_SY1000_.jpg",
     category: ["men"],
     ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
   },
-  // Agrega más productos según necesites
+  {
+    id: 11,
+    name: "Randy Orton Viper T-Shirt",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/randy-orton/mens-black-randy-orton-viper-bite-t-shirt_ss5_p-202931875+u-x8djq5bebwgij2hr4jfj+v-sby6qx8eqxpa7dotjxiu.jpg?_hv=2",
+    category: ["men"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 12,
+    name: "The Rock Final Boss T-Shirt",
+    price: 24.99,
+    image: "https://fanatics.frgimages.com/the-rock/youth-prosphere-blue-the-rock-the-final-boss-t-shirt_ss5_p-201685163+u-zabx2glfcqgp84gjenqr+v-tk3jngsggnzynvjtkecl.jpg?_hv=2",
+    category: ["men"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 13,
+    name: "LA Knight Yeah! T-Shirt",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/la-knight/wwe-la-knight-yeah-t-shirt-yellow-mens_ss5_p-200331242+u-oinauydoqnepvg3xtd7d+v-rguur7ikyynhuhk754sx.jpg?_hv=2",
+    category: ["men"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 14,
+    name: "Damian Priest All Rise T-Shirt",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/damian-priest/wwe-damian-priest-el-campeon-t-shirt-black-mens_ss5_p-201642050+u-nt2xoemt6vrjf8oeq4gv+v-pfvolye9kupkyx088rcx.jpg?_hv=2",
+    category: ["men"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 15,
+    name: "Becky Lynch The Man T-Shirt",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/becky-lynch/youth-black-becky-lynch-back-in-business-t-shirt_ss5_p-202832399+pv-1+u-dvkl3c7bjwc8q0hci7kx+v-7pslgsm3heibr6w3m5un.jpg?_hv=2&w=900",
+    category: ["women"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 16,
+    name: "Roman Reigns OTC Hoodie",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/roman-reigns/mens-black-roman-reigns-otc-pullover-hoodie_ss5_p-202104138+u-cdz4q5xkwphyzoykta3m+v-ycvxou02eoylvwkxbfjy.jpg?_hv=2&w=600",
+    category: ["men", "kids"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 17,
+    name: "Seth Rollins Visionary T-Shirt",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/seth-rollins/wwe-seth-rollins-visionary-revolutionary-t-shirt-black-mens_ss5_p-201140514+u-faalrizevddz75rtepnp+v-1bwk7opc1yjczdwkbxqp.jpg?_hv=2&w=600",
+    category: ["men"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 18,
+    name: "Cody Rhodes American Nightmare T-Shirt",
+    price: 24.99,
+    image: "https://mobile.ukevents.net/wp-content/uploads/2024/03/ss5_p-200909780-u-w6bwg4gjujhfxgkyxrvz-v-9emljt6yhg0srqvbhbfq.jpg",
+    category: ["men", "kids"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 19,
+    name: "R-Truth Ron Cena T-Shirt",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/r-truth/wwe-r-truth-ron-cena-t-shirt-red-mens_ss5_p-203086580+u-omjzsa8yrzbtsqkh0ejr+v-urbnsogsmlc5sjs3mlst.jpg?_hv=2",
+    category: ["men"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  },
+  {
+    id: 20,
+    name: "WrestleMania 41 Logo T-Shirt",
+    price: 24.99,
+    image: "https://images.footballfanatics.com/wrestlemania/mens-black-wrestlemania-41-logo-box-t-shirt_ss5_p-203038024+pv-1+u-zgiq2vk00s0gtllu7xou+v-ckygywcrpc6pjk5tz9ot.jpg?_hv=2&w=900",
+    category: ["men", "women", "kids"],
+    ratings: Array.from({ length: 5 }, () => Math.floor(Math.random() * 5) + 1)
+  }
 ];
